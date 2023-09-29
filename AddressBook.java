83fd9f6fbd7cb09222a59951d59bd80e6df0071d
yaman <mohdyaman@gmail.com>
2023-09-29 11:17:20 -0400

    I added a print method "AdressBook"

---
+++ AddressBook.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddies;
+
+    public AddressBook(){
+        buddies = new ArrayList<>();
+    }
+    public void addBuddy(BuddyInfo buddy) {
+        if(buddy != null) {
+            buddies.add(buddy);
+        }
+    }
+    public BuddyInfo removeBuddy(int index) {
+        if(index >= 0 && index < buddies.size()){
+            return buddies.remove(index);
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args){
+        BuddyInfo buddy = new BuddyInfo("yaman","carleton",613);
+        AddressBook buddies = new AddressBook();
+        System.out.println(buddy.getName());
+        System.out.println("Address Book");
+        System.out.println("Address 2");
+        buddies.addBuddy(buddy);
+        buddies.removeBuddy(0);
+    }
+}
